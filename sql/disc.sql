d9567394cc2834f26676e4756e70558b2bee9b69
Ali <74619504+Utinax@users.noreply.github.com>
2021-04-17 03:53:02 +0300

    Add files via upload

--- sql/disc.sql
+++ sql/disc.sql
@@ -1,7 +1,7 @@
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
-	('esrar_ham', 'Esrar Yaprağı', 1, 0, 1),
-	('esrar_islenmis', 'Ayıklanmış Esrar', 1, 0, 1),
-	('esrar_islenmis2', 'Kurutulmuş Esrar', 1, 0, 1),
-	('esrar_paketlenmis', 'Esrar [1g]', 1, 0, 1),
-	('poset', 'Poşet', 1, 0, 1)
+	('esrar_ham', 'Weed Leaf', 1, 0, 1),
+	('esrar_islenmis', 'Shelled Leaf', 1, 0, 1),
+	('esrar_islenmis2', 'Dried Leaf', 1, 0, 1),
+	('esrar_paketlenmis', 'Weed [1g]', 1, 0, 1),
+	('poset', 'Bag', 1, 0, 1)
 ;
